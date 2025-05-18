d387e5a8a8fd8b2512fee66b4ec92cc024e139f4
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-18 15:10:17 +0330

    Get Elements

--- src/todolist.ts
+++ src/todolist.ts
@@ -1,4 +1,7 @@
 // get elements from index.html
 const $ = document;
 const todoInput = $.querySelector(".todo-value") as HTMLInputElement;
-const AddTodo = $.querySelector(".add-todo") as HTMLButtonElement;
+const addTodo = $.querySelector(".add-todo") as HTMLButtonElement;
+const todoList = $.querySelector(".todoList") as HTMLUListElement;
+const clearAllTodo = $.querySelector(".clear-todos") as HTMLButtonElement;
+const removeTodo = $.querySelector(".icon") as HTMLButtonElement;
