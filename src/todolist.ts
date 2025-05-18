c1cd6ba3a4de0ca7ffef9f8cdbd84d2d49b4832c
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-18 15:11:54 +0330

    Create interface

--- src/todolist.ts
+++ src/todolist.ts
@@ -5,3 +5,10 @@ const addTodo = $.querySelector(".add-todo") as HTMLButtonElement;
 const todoList = $.querySelector(".todoList") as HTMLUListElement;
 const clearAllTodo = $.querySelector(".clear-todos") as HTMLButtonElement;
 const removeTodo = $.querySelector(".icon") as HTMLButtonElement;
+
+// create interface
+interface Todo {
+  id: string;
+  title: string;
+  isComplete: boolean;
+}
