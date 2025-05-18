e1a0595407bb49e882e18994142d2ec2d0b9375d
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-18 15:57:13 +0330

    Update todolist.js and install the global compiler

--- dist/todolist.js
+++ dist/todolist.js
@@ -1,26 +1,48 @@
-const todoValue = document.querySelector(".todo-value");
-const addTodo = document.querySelector(".add-todo");
-const clearTodos = document.querySelector(".clear-todos");
-const todoList = document.querySelector(".todoList");
-const handleSubmit = (event) => {
+// get elements from index.html
+const $ = document;
+const todoInput = $.querySelector(".todo-value");
+const addTodo = $.querySelector(".add-todo");
+const clearAllTodo = $.querySelector(".clear-todos");
+const removeTodo = $.querySelector(".icon");
+const todoList = $.querySelector(".todoList");
+// create todos array and ....
+const todos = JSON.parse(localStorage.getItem("todos") || "[]");
+// create add new todo Function
+const addNewTodo = (event) => {
+    // set preventDefault method
     event.preventDefault();
-    const newTodo = {
+    console.log("todos");
+    // Creating an object that conforms to the Todo interface
+    const todoObj = {
         id: crypto.randomUUID(),
-        title: todoValue.value,
+        title: todoInput.value,
         isComplete: false,
     };
-    addTodoToDom(newTodo);
-    todoValue.value = "";
-    todoValue.focus();
+    // call function
+    addTodoToDom(todoObj);
+    todos.push(todoObj);
+    setTodoInLocal();
+    // input handler
+    todoInput.value = "";
+    todoInput.focus();
 };
+// create addTodoToDom function
 const addTodoToDom = (todo) => {
-    todoList.insertAdjacentHTML("beforeend", `
-        <li>
+    todoList.insertAdjacentHTML("beforeend", ` <li>
           ${todo.title}<span class="icon"
             ><i class="fas fa-trash"></i
           ></span>
-        </li>
-    `);
+        </li>`);
 };
-addTodo.addEventListener("click", (event) => handleSubmit(event));
+// create set todo to local storage function
+const setTodoInLocal = () => {
+    // set todo at local storage
+    localStorage.setItem("todos", JSON.stringify(todos));
+};
+// set add event listener
+addTodo.addEventListener("click", (event) => addNewTodo(event));
+window.addEventListener("DOMContentLoaded", () => {
+    // set loop for render the item
+    todos.forEach((todo) => addTodoToDom(todo));
+});
 //# sourceMappingURL=todolist.js.map
